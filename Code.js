566249e191010cbc9e80e72ee45023dd253317fc
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-25 11:48:32 +0100

    update

---
+++ Code.js
@@ -0,0 +1,4 @@
+let Image1 = document.getElementById("img1")
+let Image2 = document.getElementById("img2")
+let Image3 = document.getElementById("img3")
+let Image4 = document.getElementById("img4")
