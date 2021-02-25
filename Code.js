fd638b92887071a28d35d338a19f891dee158ee3
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-25 13:41:37 +0100

    fade funcions

--- Code.js
+++ Code.js
@@ -4,15 +4,28 @@ let Image3 = document.getElementById("img3")
 let Image4 = document.getElementById("img4")
 
 var counter = 0
+var i = 0;
 
-
-
-if(counter === 0){
-    for(var i = 0; i < 100; i++){
-        Image1.style.opacity = i
-        console.log("test")
-        setTimeout
+    function fadeout()
+    {
+    setInterval(function() {
+        // Image1.style.opacity = i
+        console.log("out")
+        i--
+        }, 20)
     }
+    function fadein()
+    {
+    setInterval(function() {
+        // Image1.style.opacity = i
+        console.log("in")
+        i++
+        }, 20)
+    }
+
+
+
+
     counter++
     
-}
+// }
