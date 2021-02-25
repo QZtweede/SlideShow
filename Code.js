2bc72426353dbc77fa16c10137ebce57f3f7cf7f
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-25 14:55:37 +0100

    Update Code.js

--- Code.js
+++ Code.js
@@ -3,29 +3,36 @@ let Image2 = document.getElementById("img2")
 let Image3 = document.getElementById("img3")
 let Image4 = document.getElementById("img4")
 
-var counter = 0
-var i = 0;
+loop1()
 
-    function fadeout()
-    {
-    setInterval(function() {
-        // Image1.style.opacity = i
-        console.log("out")
-        i--
-        }, 20)
-    }
-    function fadein()
-    {
-    setInterval(function() {
-        // Image1.style.opacity = i
-        console.log("in")
-        i++
-        }, 20)
-    }
+async function loop1(){
+    Image1.classList.toggle("fade")
+    await sleep(2000)
+    Image1.classList.toggle("fade")
+    loop2()
+}
 
+async function loop2(){
+    Image2.classList.toggle("fade")
+    await sleep(2000)
+    Image2.classList.toggle("fade")
+    loop3()
+}
 
+async function loop3(){
+    Image3.classList.toggle("fade")
+    await sleep(2000)
+    Image3.classList.toggle("fade")
+    loop4()
+}
 
+async function loop4(){
+    Image4.classList.toggle("fade")
+    await sleep(2000)
+    Image4.classList.toggle("fade")
+    loop1()
+}
 
-    counter++
-    
-// }
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
