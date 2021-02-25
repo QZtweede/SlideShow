ce0d147cbbfd126c4bdc22404ec32a6a699c2177
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-02-25 11:58:51 +0100

    update

--- Code.js
+++ Code.js
@@ -2,3 +2,17 @@ let Image1 = document.getElementById("img1")
 let Image2 = document.getElementById("img2")
 let Image3 = document.getElementById("img3")
 let Image4 = document.getElementById("img4")
+
+var counter = 0
+
+
+
+if(counter === 0){
+    for(var i = 0; i < 100; i++){
+        Image1.style.opacity = i
+        console.log("test")
+        setTimeout
+    }
+    counter++
+    
+}
